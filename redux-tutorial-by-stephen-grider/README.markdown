1e9b9f7b20728f8d1a497c071ce14ad4954d399f
Russellkusuma <russellkusuma11@gmail.com>
2021-09-04 00:56:06 -0700

    react course summer 2021

---
+++ redux-tutorial-by-stephen-grider/README.markdown
@@ -0,0 +1,5 @@
+# Redux tutorial by Stephen Grider
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/russellkusuma/pen/poePppZ](https://codepen.io/russellkusuma/pen/poePppZ).
+
+
---
+++ redux-tutorial-by-stephen-grider/dist/script.js
@@ -0,0 +1,89 @@
+console.clear();
+
+// People dropping off forms
+const createPolicy = (name, amount) => {
+  return {
+    // Action(form in analogy)
+    type: "CREATE_POLICY",
+    payload: {
+      name: name,
+      amount: amount
+    }
+  };
+};
+
+const deletePolicy = (name) => {
+  return {
+    type: "DELETE_POLICY",
+    payload: {
+      name: name
+    }
+  };
+};
+
+const createClaim = (name, amountOfMoneyToCollect) => {
+  return {
+    type: "CREATE_CLAIM",
+    payload: {
+      name: name,
+      amountOfMoneyToCollect: amountOfMoneyToCollect
+    }
+  };
+};
+
+// Redudcers (Departments)
+// oldListOfClaims = [] defaults null to empty arr
+const claimsHistory = (oldListOfClaims = [], action) => {
+  if (action.type === "CREATE_CLAIM") {
+    // This action is relevant (Form)
+    // ...oldListOfClaims means join contents of two arr
+    // Make a new array/obj instead of modifiying
+    return [...oldListOfClaims, action.payload];
+  }
+  // This action is not relevant (Form)
+  return oldListOfClaims;
+};
+
+// If undefined BagOfMoney default to 100
+const accounting = (bagOfMoney = 100, action) => {
+  if (action.type === "CREATE_CLAIM") {
+    return bagOfMoney - action.payload.amountOfMoneyToCollect;
+  } else if (action.type === "CREATE_POLICY") {
+    return bagOfMoney + action.payload.amount;
+  }
+  return bagOfMoney;
+};
+
+const policies = (listOfPolicies = [], action) => {
+  if (action.type === "CREATE_POLICY") {
+    return [...listOfPolicies, action.payload.name];
+  } else if (action.type === "DELETE_POLICY") {
+    // Return a new array without the deleted name
+    return listOfPolicies.filter((name) => name !== action.payload.name);
+  }
+  return listOfPolicies;
+};
+
+const { createStore, combineReducers } = Redux;
+
+// Combine departments into an object
+const ourDepartments = combineReducers({
+  accounting: accounting,
+  claimsHistory: claimsHistory,
+  policies: policies
+});
+
+const store = createStore(ourDepartments);
+
+// Create action->dispatch->departments/reducer
+// Basically each dispatch is an entire redux cycle
+store.dispatch(createPolicy("Alex", 20));
+store.dispatch(createPolicy("Jim", 30));
+store.dispatch(createPolicy("Bob", 40));
+
+store.dispatch(createClaim("Alex", 120));
+store.dispatch(createClaim("Jim", 50));
+
+store.dispatch(deletePolicy("Bob"));
+
+console.log(store.getState());
